7f6bd398444e40d7a8e401da3548437c93111f05
Vagner de Souza <inaciovagner1@gmail.com>
2021-05-10 11:47:56 +0100

    Add project files.

---
+++ FinallyBlock/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinallyBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter value of A : ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("Enter value of B : ");
+            int B = int.Parse(Console.ReadLine());
+            try
+            {
+                int C = A / B;
+                Console.WriteLine("C = {0} / {1} = {2}", A, B, C);
+            }
+            catch (DivideByZeroException de)
+            {
+                Console.WriteLine("Can't divide a number by 0");
+            }
+            finally
+            {
+                Console.WriteLine("A = {0}\nB = {1}", A, B);
+            }
+        }
+    }
+}
